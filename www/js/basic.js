ce6e8de21bed56d8c74858f13affcdbd013f0927
Sabyasachi1916 <sabyasachi@SABYASACHIs-Mac-mini.local>
2017-03-25 19:20:46 +0530

    updated

--- www/js/basic.js
+++ www/js/basic.js
@@ -60,7 +60,8 @@ $( ".material input, .material select,.material textarea" ).focusout(function() 
 		return false;
     });
 	
-	$("ul.package li").click(function () {        
+	$("ul.package li").click(function () {    
+		console.log(localStorage.getItem("package"));    
 	  	var tab_id = $(this).attr('data-tab');
 	  	$(".tab-package").hide();
 		
@@ -71,6 +72,31 @@ $( ".material input, .material select,.material textarea" ).focusout(function() 
 		
 		$(".tab-package").removeClass('active');
 		$("#"+tab_id).addClass('active');
+
+		var parentHere = $("ul.package li").closest('div').attr('id')
+		console.log(parentHere);
+		
+		if (parentHere == "theme") {
+			localStorage.setItem("package", $(".active div").get(0).innerHTML);
+			localStorage.setItem("package_id",tab_id);
+		}
+		if (parentHere == "room_size") {
+			localStorage.setItem("room_size", $(".active div").get(0).innerHTML);
+			localStorage.setItem("package_id_roomsize",tab_id);
+		}
 	});
+	
+  
+  $(".swiper-pagination-bullet swiper-pagination-bullet-active").click(function(){
+     console.log("baseball selected");
+    });
+	
+	
+	
+	
+	
+	
+	
+	
 
 });	
